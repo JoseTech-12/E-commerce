34e8323ca5b415bb4995c3c55fd99a961c6fb905
JoseTech-12 <jp898467@gmail.com>
2026-03-14 08:09:30 -0500

    cargamos los productos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react";
 import { getProducts } from "./Service/ProductsApi";
 import type { Product } from "./Service/ProductsApi";
 import { Card } from "./Components/Card";
+import "./App.css";
 
 export const App = () => {
-const [products, setProducts] = useState<Product[]>([]);
-const [selectedCategory, setSelectedCategory] = useState<string>("all");
+  const [products, setProducts] = useState<Product[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string>("all");
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -21,34 +22,35 @@ const [selectedCategory, setSelectedCategory] = useState<string>("all");
     fetchProducts();
   }, []);
 
+  const categories = ["all", ...new Set(products.map((p) => p.category))];
 
-  const categories = ["all", ...new Set(products.map(p => p.category))];
+  const filteredProducts =
+    selectedCategory === "all"
+      ? products
+      : products.filter((p) => p.category === selectedCategory);
 
-
-   const filteredProducts = selectedCategory === "all" ? products : products.filter(p => p.category === selectedCategory);
-
-
-   console.log(products)
+  console.log(products);
   return (
     <div>
-        <h1>Categorias</h1>
-        <nav>
-          {categories.map((category) => (
-            <button
-              key={category}
-              onClick={() => setSelectedCategory(category)}
-            >
-              {category}
-            </button>
-          ))}
-        </nav>
+      <h1>Categorias</h1>
+      <nav>
+        {categories.map((category) => (
+          <button
+            className="ButtonCategory"
+            key={category}
+            onClick={() => setSelectedCategory(category)}
+          >
+            {category}
+          </button>
+        ))}
+      </nav>
 
-        <div>
-             <h2>Productos</h2>
-            {filteredProducts.map((product) => (
-              <Card key={product.id} product={product} />
-            ))}
-        </div>
+      <div>
+        <h2>Productos</h2>
+        {filteredProducts.map((product) => (
+          <Card key={product.id} product={product} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,12 +1,12 @@
-import type { Product } from "../Service/ProductsApi"
+import type { Product } from "../Service/ProductsApi";
 
 export const Card = ({ product }: { product: Product }) => {
   return (
     <div>
-     <h2>{product.title}</h2>
-     <p>Price: ${product.price.toFixed(2)}</p>
-     <p>Category: {product.category}</p>
-     <img src={product.image} alt={product.title} />
+      <h2>{product.title}</h2>
+      <p>Price: ${product.price}</p>
+      <p>Category: {product.category}</p>
+      <img src={product.image} alt={product.title} />
     </div>
-  )
-}
+  );
+};
