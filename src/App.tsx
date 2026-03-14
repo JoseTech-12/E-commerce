331df49e603a874033608bf33ad3a68c668ee3f9
JoseTech-12 <jp898467@gmail.com>
2026-03-14 07:50:07 -0500

    cargamos los productos

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { useEffect,useState } from "react"
+import { getProducts } from "./Service/ProductsApi";
+import type { Product } from "./Service/ProductsApi";
+import { Card } from "./Components/Card";
+
+export const App = () => {
+const [products, setProducts] = useState<Product[]>([]);
+const [selectedCategory, setSelectedCategory] = useState<string>("all");
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const productsData = await getProducts();
+        setProducts(productsData);
+        console.log(products);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+
+  const categories = ["all", ...new Set(products.map(p => p.category))];
+
+
+   const filteredProducts = selectedCategory === "all" ? products : products.filter(p => p.category === selectedCategory);
+
+
+   console.log(products)
+  return (
+    <div>
+        <h1>Categorias</h1>
+        <nav>
+          {categories.map((category) => (
+            <button
+              key={category}
+              onClick={() => setSelectedCategory(category)}
+            >
+              {category}
+            </button>
+          ))}
+        </nav>
+
+        <div>
+             <h2>Productos</h2>
+            {filteredProducts.map((product) => (
+              <Card key={product.id} product={product} />
+            ))}
+        </div>
+    </div>
+  )
+}
--- src/Service/ProductsApi.ts
+++ src/Service/ProductsApi.ts
@@ -1,17 +1,16 @@
-interface Product {
+export interface Product {
   id: number;
   title: string;
-    price: number;
-    description: string;
-    category: string;
-    image: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
 }   
 
 
 export const getProducts = async  (): Promise<Product[]> => {
   try {
     const response = await fetch('https://fakestoreapi.com/products');
-   
     const products = await response.json();
     return products;
   } catch (error) {
